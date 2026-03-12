eb71f31beaeae003d04607164bf0ae058ab7481c
Ridhanovita <novitaridha796@gmail.com>
2026-03-12 19:51:15 +0700

    menambahkan package

---
+++ src/main/java/com/example/ktpapi/KtpapiApplication.java
@@ -0,0 +1,12 @@
+package com.example.ktpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtpapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtpapiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/ktpapi/KtpapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
